2b0c0a16dc9bc7746778badb74e0898ba03f096e
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-03-31 21:56:23 +0300

    first green test

--- __tests__/browser.e2e.js
+++ __tests__/browser.e2e.js
@@ -1,5 +1,15 @@
-test('should be cool', async () => {
-  await page.goto('http://localhost:3000');
+test('should register players', async () => {
+  const player1 = 'Yaniv';
+  const player2 = 'Computer';
 
-  expect(await page.title()).toBe('React App');
+  await page.goto('http://localhost:3000');
+  await page.type('[data-hook="p1-input"]', player1);
+  await page.type('[data-hook="p2-input"]', player2);
+  await page.click('[data-hook="new-game"]');
+  expect(await page.$eval('[data-hook="p1-title"]', el => el.innerText)).toBe(
+    player1
+  );
+  expect(await page.$eval('[data-hook="p2-title"]', el => el.innerText)).toBe(
+    player2
+  );
 });
--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    player1: '',
+    player2: ''
+  };
   render() {
     return (
       <div className="App">
@@ -20,6 +24,21 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div>
+          <input
+            data-hook="p1-input"
+            onChange={evt => this.setState({ player1: evt.target.value })}
+          />
+          <input
+            data-hook="p2-input"
+            onChange={evt => this.setState({ player2: evt.target.value })}
+          />
+          <button data-hook="new-game">new game</button>
+        </div>
+        <div>
+          <h1 data-hook="p1-title">{this.state.player1}</h1>
+          <h1 data-hook="p2-title">{this.state.player2}</h1>
+        </div>
       </div>
     );
   }
